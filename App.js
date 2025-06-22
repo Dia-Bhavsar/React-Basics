6dbf10f23b72521ad6463fed42d212d253324e7c
Dia Bhavsar <diabhavsar16@Dias-MacBook-Air.local>
2025-06-22 12:27:23 +0530

    Chapter2 - Parcel integration

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
-const parent = React.createElement('div', { id: "parent" },
-    React.createElement('div', { id: "child" },
-        [React.createElement('h1', { id: "h1" }, "Hello Dia"),
-        React.createElement('h1', { id: "h2" }, "How are you?"),
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const parent = React.createElement('div', { id: "parent" }, [
+    React.createElement('div', { key: "child1" },
+        [
+            React.createElement('h1', { key: 'h1' }, "Hello Dia"),
+            React.createElement('h2', { key: 'h2' }, "How are you?")
         ]
-    )
-)
+        ,
+        React.createElement('div', { key: "child2" },
+            [
+                React.createElement('h1', { key: 'h3' }, "Namaste From India"),
+                React.createElement('h2', { key: 'h4' }, "I am fine, thank you!")
+            ]
+        ))])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Namste React
+# Namaste React
+
+# parcel
+
+- dev build
+- Local server (localhost://1234)
+- Hot module replacement (HMR)
+- File watching algorithum
+- Caching - Fast builds
+- Image Optimaziation
+- Minifying (prod build)
+- Building
+- Compress
+- Consistance Hashing
+- Code Spliting
+- Differential Bundlling
+- Diagnostics (Errors on console)
+- Tree shaking - Remove un-used code.
+- Https mode
+- Transpile - Very fast
+- Different dev and prod builds
